61d166b0952d1d16eeef69b6923fbf8488e52b41
HARIPRAKASH805 <hariprakash5881@gmail.com>
2026-07-21 23:15:35 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black py-8 border-t border-gray-800">
+
+      <div className="text-center text-gray-400">
+
+        <p>Designed & Developed by Hariprakash P</p>
+
+        <p className="mt-2">
+          React • Vite • TypeScript • Tailwind CSS
+        </p>
+
+        <p className="mt-2">
+          © 2026 All Rights Reserved
+        </p>
+
+      </div>
+
+    </footer>
+  );
+}
