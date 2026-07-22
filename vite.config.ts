01e9f4a9023106aaad75e019843f093484fb1182
HARIPRAKASH805 <hariprakash5881@gmail.com>
2026-07-22 12:31:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+        },
+      },
+    },
+  },
 });
